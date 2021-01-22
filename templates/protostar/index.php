e996d4e3379e81c63a408768eacf94cd2ec162c1
System Administrator <root@MacBook-Pro-de-Said.local>
2021-01-22 08:35:27 -0600

    prueba

--- templates/protostar/index.php
+++ templates/protostar/index.php
@@ -147,6 +147,7 @@ else
 ?>">
 	<!-- Body -->
 	<div class="body" id="top">
+      esto es una prueba
 		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
 			<!-- Header -->
 			<header class="header" role="banner">
