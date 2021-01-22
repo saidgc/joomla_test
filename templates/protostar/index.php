131c4c5d03e9dce700f4fac235cfcc72e7b1f11e
saidgc <said@hotmail.com>
2021-01-22 09:56:49 -0600

    agregue mi nombre al index

--- templates/protostar/index.php
+++ templates/protostar/index.php
@@ -147,6 +147,7 @@ else
 ?>">
 	<!-- Body -->
 	<div class="body" id="top">
+	said
 		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
 			<!-- Header -->
 			<header class="header" role="banner">
